c0f150ebe7368f75bd4b4b55204bac7260295e94
Osaze Oviawe <ocubed88@gmail.com>
2023-08-05 13:24:40 +0100

    feat: - added right side bar - added functionality to the onboarding [WIP]

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,48 +1,44 @@
-import '../globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import { ClerkProvider } from "@clerk/nextjs";
-
+import '../globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { ClerkProvider } from '@clerk/nextjs';
 
 import Topbar from '@/components/shared/Topbar';
 import LeftSidebar from '@/components/shared/LeftSidebar';
 import RightSidebar from '@/components/shared/RightSidebar';
 import Bottombar from '@/components/shared/Bottombar';
 
-
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Threads | Web-App',
-  description: 'A Next.js 13 Web Application for creating Threads',
-}
+	title: 'Threads | Web-App',
+	description: 'A Next.js 13 Web Application for creating Threads',
+};
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode
+	children: React.ReactNode;
 }) {
-  return (
-    <ClerkProvider>
-      <html lang="en">
-        <body className={inter.className}>
-          <Topbar />
+	return (
+		<ClerkProvider>
+			<html lang='en'>
+				<body className={inter.className}>
+					<Topbar />
 
-          <main>
-            <LeftSidebar />
+					<main className='flex'>
+						<LeftSidebar />
 
-            <section className="flex min-h-screen flex-1 flex-col items-center bg-dark-1 px-6 pb-10 pt-28 max-md:pb-32 sm:px-10">
-              <div className='w-full max-w-4xl'>
-                {children}
-              </div>
-            </section>
+						<section className='flex min-h-screen flex-1 flex-col items-center bg-dark-1 px-6 pb-10 pt-28 max-md:pb-32 sm:px-10'>
+							<div className='w-full max-w-4xl'>{children}</div>
+						</section>
 
-            <RightSidebar />
-          </main>
+						<RightSidebar />
+					</main>
 
-          <Bottombar />
-        </body>
-      </html>
-    </ClerkProvider>
-  )
+					<Bottombar />
+				</body>
+			</html>
+		</ClerkProvider>
+	);
 }
