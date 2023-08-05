cf94582b23dc86e4b8372a1eaa838ec9a481c86b
Osaze Oviawe <ocubed88@gmail.com>
2023-08-05 10:14:16 +0100

    feat: - added the bottom bar nav for mobile and tab view

--- components/shared/Bottombar.tsx
+++ components/shared/Bottombar.tsx
@@ -1,9 +1,47 @@
-import React from 'react'
+'use client';
+import { sidebarLinks } from '@/constants';
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import { usePathname, useRouter } from 'next/navigation';
+import { SignOutButton, SignedIn } from '@clerk/nextjs';
 
 function Bottombar() {
-  return (
-	<div>Bottombar</div>
-  )
+	const router = useRouter();
+	const pathname = usePathname();
+
+	return (
+		<section className='fixed bottom-0 z-10 w-full rounded-t-3xl bg-glassmorphism p-4 backdrop-blur-lg xs:px-7 md:hidden'>
+			<div className='flex items-center justify-between gap-3 xs:gap-5'>
+				{sidebarLinks.map((link) => {
+					const isActive =
+						(pathname.includes(link?.route) &&
+							link?.route.length > 1) ||
+						pathname === link?.route;
+					return (
+						<Link
+							href={link?.route}
+							key={link?.label}
+							className={`${
+								isActive && 'bg-primary-500'
+							} relative flex flex-col items-center gap-2 rounded-lg p-2 sm:flex-1 sm:px-2 sm:py-2.5`}
+						>
+							<Image
+								src={link?.imgURL}
+								alt={link?.label}
+								width={24}
+								height={24}
+							/>
+
+							<p className='text-subtle-medium text-light-1 max-sm:hidden'>
+								{link?.label.split(/\s+/)[0]}
+							</p>
+						</Link>
+					);
+				})}
+			</div>
+		</section>
+	);
 }
 
-export default Bottombar
+export default Bottombar;
